aa39079d1d7082d669a0c6fb85a47c1b9f378093
mbaridonn <mbaridonn@gmail.com>
2019-05-06 01:29:45 -0300

    Created a test case regarding the POST of a new pokemon

--- src/test/java/tests/PokemonControllerTests.java
+++ src/test/java/tests/PokemonControllerTests.java
@@ -2,59 +2,60 @@ package tests;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import org.json.JSONException;
+import static org.hamcrest.Matchers.*;
 
 import main.PokemonApplication;
-import main.PokemonRepository;
+import model.Pokemon;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = PokemonApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = PokemonApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class PokemonControllerTests {
 	
-	@LocalServerPort
-	private int port;
-
-	TestRestTemplate restTemplate = new TestRestTemplate();
-
-	HttpHeaders headers = new HttpHeaders();
-
+	@Autowired
+    private MockMvc mvc;
+	
 	@Test
-	public void testGETPokemons() throws JSONException {
-
-		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-
-		ResponseEntity<String> response = restTemplate.exchange(
-				createURLWithPort("/pokemons"),
-				HttpMethod.GET, entity, String.class);
-
-		String expected = "[{id:1,name:Charmander,type:fire},"
-				+ "{id:2,name:Squirtle,type:water},"
-				+ "{id:3,name:Bulbasaur,type:plant}]";
-
-		JSONAssert.assertEquals(expected, response.getBody(), false);
-	}
-
-	private String createURLWithPort(String uri) {
-		return "http://localhost:" + port + uri;
+	public void getPokemons() throws Exception {
+	 
+	    Pokemon charmander = new Pokemon("Charmander","fire");
+	    Pokemon squirtle = new Pokemon("Squirtle","water");
+	    Pokemon bulbasaur = new Pokemon("Bulbasaur","plant");
+	 
+	    mvc.perform(get("/pokemons")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isOk())
+	      .andExpect(content()
+	      .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	      .andExpect(jsonPath("$[0].name", is(charmander.getName())))
+	      .andExpect(jsonPath("$[1].name", is(squirtle.getName())))
+	      .andExpect(jsonPath("$[2].name", is(bulbasaur.getName())));
 	}
 	
+	@Test
+	public void postPokemon() throws Exception {
+		
+		Pokemon pikachu = new Pokemon("Pikachu", "electric");
+		byte[] pikachuJson = toJson(pikachu);
+		
+		mvc.perform(post("/pokemons")
+				.content(pikachuJson)
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().is2xxSuccessful()); //should be 201?
+	}
+	
+	private byte[] toJson(Object r) throws Exception {
+        ObjectMapper map = new ObjectMapper();
+        return map.writeValueAsString(r).getBytes();
+    }
 	
 }
