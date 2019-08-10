5afbd2378befeec5bad626095554704c4baa2e3d
mbaridonn <mbaridonn@gmail.com>
2019-08-09 23:33:22 -0300

    Created voting component and routes

--- client/pokemon-app/src/app/app.module.ts
+++ client/pokemon-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { MatMenuModule, MatButtonModule, MatIconModule, MatCardModule, MatToolba
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
@@ -11,6 +12,13 @@ import { NewPokemonDialogComponent, PokemonDialog } from './new-pokemon-dialog/n
 import { FormsModule } from '@angular/forms';
 import { PokemonsRepository } from 'src/repositories/pokemons-repository';
 import { PokemonsService } from 'src/services/pokemons.service';
+import { VotingComponent } from './voting/voting.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {path: 'vote', component: VotingComponent},
+  {path: '', component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
@@ -18,10 +26,13 @@ import { PokemonsService } from 'src/services/pokemons.service';
     PokemonCardComponent,
     PokemonsComponent,
     NewPokemonDialogComponent,
-    PokemonDialog
+    PokemonDialog,
+    VotingComponent,
+    HomeComponent
   ],
   entryComponents: [NewPokemonDialogComponent, PokemonDialog],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     BrowserAnimationsModule,
     MatMenuModule,
