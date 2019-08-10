455a20c539becfc678224afd2d170a3b5584af7b
mbaridonn <mbaridonn@gmail.com>
2019-08-10 00:31:22 -0300

    Two random pokemons are retrieved in the client

--- client/pokemon-app/src/app/voting/voting.component.ts
+++ client/pokemon-app/src/app/voting/voting.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonsService } from 'src/services/pokemons.service';
+import { IPokemon } from 'src/model/ipokemon';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-voting',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotingComponent implements OnInit {
 
-  constructor() { }
+  private pokemons: IPokemon[];
+  errorMessage: String;
+
+  constructor(private pokemonsService: PokemonsService) { }
 
   ngOnInit() {
+    this.pokemonsService.getRandomPokemons().subscribe(
+      pokemons => {this.pokemons = pokemons},
+      error => this.errorMessage = <any>error
+    );
   }
 
 }
