dd7ee978ca47d4bbaf551cfb9aacd0deb9a4ebfe
mbaridonn <mbaridonn@gmail.com>
2019-09-19 23:42:46 -0300

    Created basic table showing scores

--- client/pokemon-app/src/app/leaderboard/leaderboard.component.ts
+++ client/pokemon-app/src/app/leaderboard/leaderboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IPokemon } from 'src/model/ipokemon';
+import { PokemonsService } from 'src/services/pokemons.service';
 
 @Component({
   selector: 'app-leaderboard',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeaderboardComponent implements OnInit {
 
-  constructor() { }
+  private pokemons: IPokemon[];
+
+  constructor(private pokemonsService: PokemonsService) { }
 
   ngOnInit() {
+    this.pokemonsService.getPokemons().subscribe(
+      pokemons => {this.pokemons = pokemons}
+    );
+  }
+
+  getScore(pokemon: IPokemon){
+    var score = pokemon.turnsWon / pokemon.turnsPlayed * 5; //score should be between 0 and 5 stars
+    var score = score || 0; //fixes if it's Nan
+    var scoreRounded = score.toFixed(2); //displays two decimals
+    return scoreRounded || 0;
   }
 
 }
